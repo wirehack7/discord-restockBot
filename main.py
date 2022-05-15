ea53915e24da15010fcd968cd52f60a57f22e607
wirehack7 <markusfritz87@gmail.com>
2022-05-15 16:38:21 +0200

    Added README.md

---
+++ main.py
@@ -0,0 +1,30 @@
+# main.py
+import os
+import random
+
+from discord.ext import commands
+import logging
+import configparser
+
+config = configparser.ConfigParser()
+
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.command(name='99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+
+bot.run(TOKEN)
